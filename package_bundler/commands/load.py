4927e3ce9d2df684b5499632f893d1f79f1cdf6e
chadrien <chadrien@chadrien.fr>
2013-07-13 16:07:02 +0200

    Show currently loaded bundle in the bundle list

--- package_bundler/commands/load.py
+++ package_bundler/commands/load.py
@@ -5,6 +5,7 @@ from ..settings import settings_filename
 
 class PackageBundlerLoadCommand(sublime_plugin.WindowCommand):
   def run(self):
+    self.settings = sublime.load_settings(settings_filename())
     bundles = self.get_bundles_list()
 
     if len(bundles) == 0:
@@ -14,14 +15,27 @@ class PackageBundlerLoadCommand(sublime_plugin.WindowCommand):
     self.window.show_quick_panel(bundles, self.chosen)
 
   def get_bundles_list(self):
-    settings = sublime.load_settings(settings_filename())
-
-    if not settings.get('bundles'):
+    if not self.settings.get('bundles'):
       return []
 
     bundles = list(map(lambda bundle:
       bundle[0]
-    , settings.get('bundles').items()))
+    , self.settings.get('bundles').items()))
+
+    bundles = self.define_current_bundle(bundles)
+
+    return bundles
+
+  def define_current_bundle(self, bundles):
+    loaded_bundle = self.settings.get('loaded_bundle')
+
+    if not loaded_bundle:
+      return bundles
+
+    for i in range(len(bundles)):
+      if bundles[i] == loaded_bundle:
+        bundles[i] += ' (Current)'
+        break
 
     return bundles
 
@@ -35,17 +49,19 @@ class PackageBundlerLoadCommand(sublime_plugin.WindowCommand):
     self.get_bundle(picked)
 
   def get_bundle(self, name):
-
     bundle = self.settings.get('bundles')[name]
 
     self.write_ignored_packages(bundle)
+    self.save_loaded_package(name)
 
     sublime.status_message('Package Bundle: bundle '+name+' loaded')
 
-  def write_ignored_packages(self, bundle):
-    settings = sublime.load_settings(settings_filename())
-
+  def write_ignored_packages(self, bundle):    
     if bundle['ignored_packages']:
       settings = sublime.load_settings('Preferences.sublime-settings')
       settings.set('ignored_packages', bundle['ignored_packages'])
       sublime.save_settings('Preferences.sublime-settings')
+
+  def save_loaded_package(self, name):
+    self.settings.set('loaded_bundle', name)
+    sublime.save_settings(settings_filename())
