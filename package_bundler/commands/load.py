1134bdcc9e0330a8621f952d6025c5062b97b3e5
chadrien <chadrien@chadrien.fr>
2013-07-13 16:42:27 +0200

    Reintented with 4 spaces

--- package_bundler/commands/load.py
+++ package_bundler/commands/load.py
@@ -4,64 +4,64 @@ import sublime_plugin
 from ..settings import settings_filename
 
 class PackageBundlerLoadCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    self.settings = sublime.load_settings(settings_filename())
-    bundles = self.get_bundles_list()
+    def run(self):
+        self.settings = sublime.load_settings(settings_filename())
+        bundles = self.get_bundles_list()
 
-    if len(bundles) == 0:
-      sublime.error_message('Package Bundler Error: No bundle to load')
-      return
+        if len(bundles) == 0:
+            sublime.error_message('Package Bundler Error: No bundle to load')
+        return
 
-    self.window.show_quick_panel(bundles, self.chosen)
+        self.window.show_quick_panel(bundles, self.chosen)
 
-  def get_bundles_list(self):
-    if not self.settings.get('bundles'):
-      return []
+    def get_bundles_list(self):
+        if not self.settings.get('bundles'):
+            return []
 
-    bundles = list(map(lambda bundle:
-      bundle[0]
-    , self.settings.get('bundles').items()))
+        bundles = list(map(lambda bundle:
+            bundle[0]
+            , self.settings.get('bundles').items()))
 
-    bundles = self.define_current_bundle(bundles)
+        bundles = self.define_current_bundle(bundles)
 
-    return bundles
+        return bundles
 
-  def define_current_bundle(self, bundles):
-    loaded_bundle = self.settings.get('loaded_bundle')
+    def define_current_bundle(self, bundles):
+        loaded_bundle = self.settings.get('loaded_bundle')
 
-    if not loaded_bundle:
-      return bundles
+        if not loaded_bundle:
+            return bundles
 
-    for i in range(len(bundles)):
-      if bundles[i] == loaded_bundle:
-        bundles[i] += ' (Current)'
-        break
+        for i in range(len(bundles)):
+            if bundles[i] == loaded_bundle:
+                bundles[i] += ' (Current)'
+                break
 
-    return bundles
+        return bundles
 
-  def chosen(self, picked):
-    if picked == -1:
-      return
+    def chosen(self, picked):
+        if picked == -1:
+            return
 
-    bundles = self.get_bundles_list()
-    picked  = bundles[picked]
+        bundles = self.get_bundles_list()
+        picked  = bundles[picked]
 
-    self.get_bundle(picked)
+        self.get_bundle(picked)
 
-  def get_bundle(self, name):
-    bundle = self.settings.get('bundles')[name]
+    def get_bundle(self, name):
+        bundle = self.settings.get('bundles')[name]
 
-    self.write_ignored_packages(bundle)
-    self.save_loaded_package(name)
+        self.write_ignored_packages(bundle)
+        self.save_loaded_package(name)
 
-    sublime.status_message('Package Bundle: bundle '+name+' loaded')
+        sublime.status_message('Package Bundle: bundle '+name+' loaded')
 
-  def write_ignored_packages(self, bundle):    
-    if bundle['ignored_packages']:
-      settings = sublime.load_settings('Preferences.sublime-settings')
-      settings.set('ignored_packages', bundle['ignored_packages'])
-      sublime.save_settings('Preferences.sublime-settings')
+    def write_ignored_packages(self, bundle):    
+        if bundle['ignored_packages']:
+            settings = sublime.load_settings('Preferences.sublime-settings')
+            settings.set('ignored_packages', bundle['ignored_packages'])
+            sublime.save_settings('Preferences.sublime-settings')
 
-  def save_loaded_package(self, name):
-    self.settings.set('loaded_bundle', name)
-    sublime.save_settings(settings_filename())
+    def save_loaded_package(self, name):
+        self.settings.set('loaded_bundle', name)
+        sublime.save_settings(settings_filename())
