eecf19a37d75313b0d0ff56f5844870d6f5d1a45
Hadrien Collongues <chadrien@chadrien.fr>
2014-02-10 13:57:03 +0100

    Added missing quote - closes #7

--- package_bundler/settings.py
+++ package_bundler/settings.py
@@ -4,4 +4,4 @@ def pb_settings_filename():
 def st_settings_filename():
     if int(sublime.version()) >= 2174:
         return 'Preferences.sublime-settings'
-    return 'Global.sublime-settings
+    return 'Global.sublime-settings'
